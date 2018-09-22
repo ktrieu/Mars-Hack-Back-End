556935932443055acfae847f584e36dad638ecfe
Kevin Trieu <dragn194@gmail.com>
2018-09-22 15:10:26 -0400

    Add a trailing slash to the url paths
    Apparently paths with no trailing slash redirect to a leading slash but not vice versa

--- Mars-Hack-Back-End/Mars_Hack_Back_End/urls.py
+++ Mars-Hack-Back-End/Mars_Hack_Back_End/urls.py
@@ -16,11 +16,11 @@ admin.autodiscover()
 urlpatterns = [
     path(r'', app.views.home),
     path(r'admin', admin.site.urls),
-    path(r'api/load_user/<cust_id>', app.views.load_user_from_api),
-    path(r'api/get_user/<cust_id>', app.views.get_user),
-    path(r'api/get_products', app.views.get_products),
-    path(r'api/create_order', app.views.create_order),
-    path(r'api/get_order/<order_id>', app.views.get_order),
-    path(r'api/get_orders', app.views.get_orders),
-    path(r'api/random_order', app.views.demo_create_random_order)
+    path(r'api/load_user/<cust_id>/', app.views.load_user_from_api),
+    path(r'api/get_user/<cust_id>/', app.views.get_user),
+    path(r'api/get_products/', app.views.get_products),
+    path(r'api/create_order/', app.views.create_order),
+    path(r'api/get_order/<order_id>/', app.views.get_order),
+    path(r'api/get_orders/', app.views.get_orders),
+    path(r'api/random_order/', app.views.demo_create_random_order)
 ]
