854fd5ec46b763a80a0fa9a4933735f6ca0aba66
Kevin Trieu <dragn194@gmail.com>
2018-09-21 23:47:42 -0400

    Add user model

--- Mars-Hack-Back-End/app/models.py
+++ Mars-Hack-Back-End/app/models.py
@@ -4,4 +4,15 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    
+    customer_id = models.CharField(max_length=73)
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+
+    income = models.IntegerField()
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=7)
+    longitude = models.DecimalField(max_digits=9, decimal_places=7)
