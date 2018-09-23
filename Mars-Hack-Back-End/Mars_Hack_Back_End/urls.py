b76128d2afbb4b6b7534e9d4bb52a0827238824f
Kevin Trieu <dragn194@gmail.com>
2018-09-22 22:24:12 -0400

    Fix urlconf typo

--- Mars-Hack-Back-End/Mars_Hack_Back_End/urls.py
+++ Mars-Hack-Back-End/Mars_Hack_Back_End/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path(r'api/get_cust_orders/<cust_id>/', app.views.cust_orders),
     path(r'api/random_order/', app.views.demo_create_random_order),
     path(r'api/group_orders/', app.views.group_orders),
-    path(r'api/get_cust_orders_merged/<cust_id>/', app.views.cust_orders_merged)
+    path(r'api/get_cust_orders_merged/<cust_id>/', app.views.cust_orders_merged),
     path(r'api/complete_order/<cust_id>/<order_id>/', app.views.complete_order)
 ]
